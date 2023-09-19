fa5ef954ee844c86cf88fefe6e26aa85780b348e
sdsanchezm <sdsanchezm@gmail.com>
2023-09-19 16:17:06 -0500

    adding the BlogPost Dto

---
+++ blogpost/Dto/BlogPostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace blogpost.Dto
+{
+    public class BlogPostDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
