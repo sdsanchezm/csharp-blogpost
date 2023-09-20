291fad231e4a9030d2da57e893b86eb87a6dce1b
sdsanchezm <sdsanchezm@gmail.com>
2023-09-20 13:14:34 -0500

    created the PostAuthorService, and Interface for the PostAuthor

---
+++ blogpost/Interfaces/IPostAuthorService.cs
@@ -0,0 +1,13 @@
+﻿using blogpost.Models;
+
+namespace blogpost.Interfaces
+{
+    public interface IPostAuthorService
+    {
+        ICollection<PostAuthor> GetPostAuthors();
+        PostAuthor GetPostAuthor(int postAuthorId);
+        bool PostAuthorExist(int postAuthorId);
+        ICollection<PostAuthor> GetPostAuthorByBlogPostId(int blogPostId);
+        ICollection<BlogPost> GetBlogPostByPostAuthor(int postAuthorId);
+    }
+}
---
+++ blogpost/Services/PostAuthorService.cs
@@ -0,0 +1,46 @@
+﻿using blogpost.Data;
+using blogpost.Interfaces;
+using blogpost.Models;
+
+namespace blogpost.Services
+{
+    public class PostAuthorService : IPostAuthorService
+    {
+        private readonly DataContext _context;
+
+        public PostAuthorService(DataContext postAuthorService)
+        {
+            _context = postAuthorService;
+        }
+
+        public ICollection<BlogPost> GetBlogPostByPostAuthor(int postAuthorId)
+        {
+            var bp =_context.BlogPostPostauthors_dbs.Where(p => p.PostAuthorJT.Id == postAuthorId).Select(bp => bp.BlogPostJT).ToList();
+            return bp;
+        }
+
+        public PostAuthor GetPostAuthor(int postAuthorId)
+        {
+            var pa = _context.PostAuthors_dbs.Where(p => p.Id == postAuthorId).FirstOrDefault();
+            return pa;
+        }
+
+        public ICollection<PostAuthor> GetPostAuthorByBlogPostId(int blogPostId)
+        {
+            var bp = _context.BlogPostPostauthors_dbs.Where(p => p.BlogPostJT.Id == blogPostId).Select(pa => pa.PostAuthorJT).ToList();
+            return bp;
+        }
+
+        public ICollection<PostAuthor> GetPostAuthors()
+        {
+            var pa = _context.PostAuthors_dbs.ToList();
+            return pa;
+        }
+
+        public bool PostAuthorExist(int postAuthorId)
+        {
+            var p = _context.PostAuthors_dbs.Any(p => p.Id == postAuthorId);
+            return p;
+        }
+    }
+}
